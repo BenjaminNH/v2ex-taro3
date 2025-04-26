0214ae6d94996aa31a1effbddc4c481dceefc551
BenjaminNH <1249376374@qq.com>
2025-04-26 11:48:59 +0800

    feat: 配置tabbar并创建节点和热门的页面结构

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,36 @@
 export default defineAppConfig({
-  pages: ["pages/index/index", "pages/thread_detail/thread_detail"],
+  pages: [
+    "pages/index/index",
+    "pages/thread_detail/thread_detail",
+    "pages/hot/hot",
+    "pages/nodes/nodes",
+  ],
+  tabBar: {
+    list: [
+      {
+        iconPath: "resource/latest.png",
+        selectedIconPath: "resource/lastest_on.png",
+        pagePath: "pages/index/index",
+        text: "最新",
+      },
+      {
+        iconPath: "resource/hotest.png",
+        selectedIconPath: "resource/hotest_on.png",
+        pagePath: "pages/hot/hot",
+        text: "热门",
+      },
+      {
+        iconPath: "resource/node.png",
+        selectedIconPath: "resource/node_on.png",
+        pagePath: "pages/nodes/nodes",
+        text: "节点",
+      },
+    ],
+    color: "#000",
+    selectedColor: "#56abe4",
+    backgroundColor: "#fff",
+    borderStyle: "white",
+  },
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
---
+++ src/pages/nodes/nodes.jsx
@@ -0,0 +1,5 @@
+import Loading from "../../components/loading";
+
+export default function Nodes() {
+  return <Loading />;
+}
